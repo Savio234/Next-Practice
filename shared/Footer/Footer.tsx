d7bd419ffe713e4c70c3151feb5f0cc3d2daa7ea
Dominic <dominicezeifejafor@gmail.com>
2024-01-11 22:43:32 +0100

    changes

--- shared/Footer/Footer.tsx
+++ shared/Footer/Footer.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Footer = () => {
+    const [imageSrc, setImageSrc] = useState<string>("/svgs/lightModeToggle.svg");
+    const handleToggle = () => {
+		setImageSrc((currentSrc) =>
+		currentSrc === "/svgs/lightModeToggle.svg" ? "/svgs/DarkModeToggle.svg" : "/svgs/lightModeToggle.svg")
+		console.log(imageSrc);
+	};
     return (
         <div>Footer</div>
     )
