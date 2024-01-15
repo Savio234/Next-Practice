8ab47005efc904efac3b89e375588e3b912f1637
Dominic <dominicezeifejafor@gmail.com>
2024-01-15 12:01:50 +0100

    Added layout folder

---
+++ layout/Main/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React, {useEffect} from 'react'
+import styles from './MainLayout.module.css'
+
+const MainLayout = () => {
+    // useEffect(() => {
+	// 	let bgColour = document.body.style;
+	// 	bgColour.backgroundColor =
+	// 	type === "soft" && route !== "/alcohol" ? "#540068" : "#E77644";
+	// });
+  return (
+    <div>MainLayout</div>
+  )
+}
+
+export default MainLayout
